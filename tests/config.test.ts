3eb56a4bbc376cd0a766a69a104928e979b1f2d2
fesmjke <fatalerror.sm@gmail.com>
2020-08-21 23:58:04 +0300

    test: added config tests

---
+++ tests/config.test.ts
@@ -0,0 +1,13 @@
+import Config from "../src/utils/config";
+
+describe('Config',() => {
+    const config = new Config();
+    it('get token that not equals undefined or \'\'',() => {
+        expect(config.token).not.toBe(undefined)
+        expect(config.token).not.toBe('')
+    })
+    it('get port number that not equals undefined or \'\'',() => {
+        expect(config.port).not.toBe(undefined)
+        expect(config.port).not.toBe('')
+    })
+})
