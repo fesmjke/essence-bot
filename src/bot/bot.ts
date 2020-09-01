7d702b9e8e6c555b4900cf3bacb3698c2c29d699
fesmjke <fatalerror.sm@gmail.com>
2020-09-01 03:02:52 +0300

    added middlewares for bot checking and response time information

--- src/bot/bot.ts
+++ src/bot/bot.ts
@@ -2,14 +2,19 @@ import Telegraf from "telegraf";
 import Config from "../settings/config"
 import CommandsComposer from "../handlers/commands";
 import MiddlewareComposer from "../handlers/middleware";
+import isBot from "../utils/is-bot";
+import respTime from "../utils/response-time";
 
 const config = Config.getInstance()
 
 const bot = new Telegraf(config.token);
+
+bot.use(isBot)
+bot.use(respTime)
 
 bot.use(
     CommandsComposer,
     MiddlewareComposer
 )
 
-export default bot;
+export default bot; 
